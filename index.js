4821756efb0b31fb97bbd38ca8dfae10cb2c07e3
Gokuldesign95 <127943397+Gokuldesign95@users.noreply.github.com>
2023-03-15 07:02:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Navbar Section Start
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+// Navbar Section End
